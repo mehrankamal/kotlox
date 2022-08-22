0303eb586f7e71903ade9fc5dbe3d34c14535300
Wajahat Ali <k180363@nu,edu.pk>
2022-08-22 23:04:55 +0500

    Added TokenType Enum and Token class Added Error Handling Functions in main Todo: 4.3 regular-languages-and-expressions

--- src/main/kotlin/Lox.kt
+++ src/main/kotlin/Lox.kt
@@ -3,9 +3,9 @@ import java.io.File
 import java.io.IOException
 import java.io.InputStreamReader
 import java.nio.charset.Charset
-import kotlin.jvm.Throws
 import kotlin.system.exitProcess
 
+var hadError = false
 fun main(args: Array<String>) {
 
     when (args.size) {
@@ -28,6 +28,8 @@ fun runREPL() {
 
         val line = streamReader.readLine() ?: break
         run(line)
+        
+        hadError = false
     }
 }
 
@@ -35,10 +37,28 @@ fun runREPL() {
 fun runFile(filePath: String) {
     val bytes = File(filePath).readBytes()
     run(String(bytes, Charset.defaultCharset()))
+
+    if (hadError) {
+        exitProcess(65)
+    }
 }
 
 
-
 fun run(source: String) {
+    val scanner = Scanner(source)
+    val tokens = scanner.scanTokens();
 
+    for (token in tokens) {
+        println(token)
+    }
+    print(source)
+}
+
+fun error(line: Int, message: String) {
+    report(line, "", message)
+}
+
+fun report(line: Int, where: String, message: String) {
+    System.err.println("[line: $line] Error $where: $message")
+    hadError = true
 }
